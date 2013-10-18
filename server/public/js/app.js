1c0800e378fc1417a3278293548a0a2c7c60cb07
Justin Merz <jrmerz@gmail.com>
2013-10-18 10:34:04 -0700

    quick updates

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -30,7 +30,11 @@ CERES.app = (function() {
 		// track all hash updates
 		if( window.gas ) {
 		    if( hash.length > 0 && cText != hash[1] ) {
-		        gas('send', 'event', 'navigation', 'text_query', { text: hash[1]});
+		        gas('send', 'event', {
+		            'eventCategory': 'navigation',
+		            'eventAction': 'text_query',
+		            'eventValue': hash[1]
+		          });
 		        cText = hash[1];
 		    }
 		    gas('send', 'pageview', window.location.pathname+window.location.hash);
